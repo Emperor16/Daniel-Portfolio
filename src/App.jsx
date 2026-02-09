5cea21433c24c718cfb047955928f67661f7f4ac
Ayodele Daniel Olamide <olamided116@gmail.com>
2026-02-09 18:32:45 +0100

    Project setup successfully, pushed only readme file to remote repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='min-h-screen overflow-x-hidden'>
+      <navbar/>
+      <main>
+        
+      </main>
+    </div>
+  )
+}
+
+export default App
