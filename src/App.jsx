7e0aadfc108bd8708b6d45e2093c940b4f12d6d9
Ayodele Daniel Olamide <olamided116@gmail.com>
2026-02-11 17:07:51 +0100

    Added the  navbar section with animation for viewing on mobile screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Hero } from "@/sections/Hero"
+import { Navbar } from "@/layout/navbar"
+import { About } from "@/sections/About"
+import { Project } from "@/sections/Project"
+import { Testimonials } from "@/sections/Testimonials"
+import { Contact } from "@/sections/Contact"
+import { Experience } from "@/sections/Experience"
 
 function App() {
   return (
     <div className='min-h-screen overflow-x-hidden'>
-      <navbar/>
+      <Navbar/>
       <main>
-        
+        <Hero/>
+        <About/>
+        <Project/>
+        <Experience/>
+        <Testimonials/>
+        <Contact/>
       </main>
     </div>
   )
--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,3 +1,3 @@
-export const Hero = () =>{
+export const Hero = () => {
     return <section></section>;
 }
