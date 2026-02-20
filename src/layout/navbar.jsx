022e12cde70aeb4085da2d3124969b0c5d63ba66
Ayodele Daniel Olamide <olamided116@gmail.com>
2026-02-20 16:53:52 +0100

    Hero section 90% completed

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -19,7 +19,7 @@ export const Navbar = () => {
           href="#"
           className="text-xl font-bold tracking-tight hover:text-primary"
         >
-          PM <span className="text-primary">.</span>
+          DAN <span className="text-primary">.</span>
         </a>
 
         {/* Desktop nav */}
