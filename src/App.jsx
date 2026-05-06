2231c99226efb2078376dfd12fce172966f73d39
Daniel Ayodele <olamided116@gmail.com>
2026-05-06 16:58:33 +0100

    Fixed deployment errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Hero } from "@/sections/hero"
 import { Navbar } from "@/layout/navbar"
-import { About } from "@/sections/About"
-import { Project } from "@/sections/Project"
-import { Testimonials } from "@/sections/Testimonials"
-import { Contact } from "@/sections/Contact"
-import { Experience } from "@/sections/Experience"
+import { About } from "@/sections/about"
+import { Project } from "@/sections/aroject"
+import { Testimonials } from "@/sections/aestimonials"
+import { Contact } from "@/sections/contact"
+import { Experience } from "@/sections/experience"
 import { Footer } from './layout/footer'
 
 function App() {
